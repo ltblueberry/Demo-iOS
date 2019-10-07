c834df7d766c01e1175cadbcdf79f494f97420ed
Andrey Romanov <aromanov.app@gmail.com>
2019-10-07 11:30:36 +0300

    initial commit

---
+++ Demo-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo-iOS
+//
+//  Created by Andreas on 07/10/2019.
+//  Copyright © 2019 ltblueberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
